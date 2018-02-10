b998dea5bd29073af6a60e278c36513864b2a8f8
Onildo Aguiar <onildo.aguiar@gmail.com>
2018-02-10 17:55:40 -0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# image-thumbnail
+# About
 Generate an image thumbnail.
+
+[![NPM](https://nodei.co/npm/image-thumbnail.png)](https://nodei.co/npm/image-thumbnail/)
+
+## Purpose?
+
+This module will generate an image thumbnail.
+
+## Usage
+
+#### Default values
+```js
+const obfuscatorEmail = require('obfuscator-email');
+
+// Default values
+const obfuscateEmail = obfuscatorEmail('onildo.aguiar@gmail.com');
+```
+
+#### With options
+```js
+const obfuscatorEmail = require('obfuscator-email');
+
+// With options
+const options = { numberOfAsteriks: 3, numberOfCharacters: 3 };
+const obfuscateEmail = obfuscatorEmail('onildo.aguiar@gmail.com', options);
+```
+
+# License
+This project is licensed under the MIT License - see the LICENSE.md file for details
---
+++ src/index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const sizeOf = require('image-size');
+const sharp = require('sharp');
+const validator = require('validator');
+const PERCENTAGE = 3;
+
+module.exports = async function (source, options) {
+    let percentage = options && options.percentage ? options.percentage : PERCENTAGE;
+    let width = options && options.width ? options.width : undefined;
+    let height = options && options.height ? options.height : undefined;
+
+
+
+
+    // check if a valid base64
+    if (validator.isBase64(base64Data)) {
+        var image;
+
+        var thumbnailImage;
+
+        var imageWidth, imageHeight;
+
+        var bufferImage = Buffer.from(base64Data, 'base64');
+
+        var dimensions = sizeOf(bufferImage);
+
+        var imageBase64;
+
+        imageWidth = getNewDimensions(percentageOfImage, dimensions.width);
+        imageHeight = getNewDimensions(percentageOfImage, dimensions.height);
+
+        // resize original image
+        imageBase64 = await sharpResize(bufferImage, imageWidth, imageHeight);
+
+        return image;
+    } else {
+        return 500;
+    }
+};
+
+const getNewDimensions = (percentageOfImage, dimension) => {
+    var compressionDimension = dimension * (percentageOfImage / 100);
+    return parseInt(compressionDimension.toFixed(0));
+}
+
+const sharpResize = (bufferImage, imageWidth, imageHeight) => {
+    return new Promise(function (resolve, reject) {
+        sharp(bufferImage)
+            .resize(imageWidth, imageHeight)
+            .withoutEnlargement()
+            .toBuffer((err, data, info) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    imageBase64 = data.toString('base64');
+                    const { format, width, height, size } = info;
+                    const imagePayload = {
+                        image: imageBase64,
+                        format: format,
+                        width: width,
+                        height: height,
+                        size: size,
+                    };
+                    resolve(imageBase64);
+                }
+            });
+    });
+};
